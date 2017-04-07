1ef6909f7aa694b72af94f06e9b0f45fbcbb9cb2
empyrean <1985226606>
2017-04-07 17:51:31 +0800

    reflect

--- financial_system/src/main/java/com.el/to/UserTO.java
+++ financial_system/src/main/java/com.el/to/UserTO.java
@@ -15,12 +15,12 @@ public class UserTO {
         this.password = password;
     }
 
-  /*  public String getUserName() {
+    public String getUserName() {
 
         return userName;
     }
 
     public void setUserName(String userName) {
         this.userName = userName;
-    }*/
+    }
 }
--- financial_system/src/test/java/com.el/ReflectTest.java
+++ financial_system/src/test/java/com.el/ReflectTest.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -15,7 +16,7 @@ public class ReflectTest {
     Logger LOG = LoggerFactory.getLogger(this.getClass());
 
     @Test
-    public void test() throws IllegalAccessException, NoSuchMethodException, ClassNotFoundException {
+    public void test() throws IllegalAccessException, NoSuchMethodException, ClassNotFoundException, InvocationTargetException {
         UserTO userTO = new UserTO();
         userTO.setPassword("111111");
        // userTO.setUserName("ak_gcc");
@@ -25,25 +26,32 @@ public class ReflectTest {
         //获取声明 fields
         //Field[] fields = aClass.getDeclaredFields();
         for (Field field : fields) {
+            //运行时赋值
             field.setAccessible(true);
             field.set(userTO,"hhhhhh");
             LOG.info("{}", field.get(userTO));
             LOG.info("{}", field.getName());
         }
-        Method[] methods = aClass.getMethods();
+        LOG.info("*******************************************");
+        Method[] methods = aClass.getDeclaredMethods();
+       // Method[] methods = aClass.getMethods();
         for (Method method : methods) {
+            //调用函数
             LOG.info("method:{}",method);
             LOG.info("methodName:{}",method.getName().startsWith("get"));
+            if(method.getName().startsWith("set")){
+                LOG.info("setValue:{}",method.invoke(userTO,"hhhhhhhhhhhhhh"));
+            }
+            if(method.getName().startsWith("get")){
+                LOG.info("getValue:{}",method.invoke(userTO));
+               // System.out.println("getValue:" + method.invoke(userTO));
+            }
         }
 
-        Class clazz = null;
-        clazz.forName("com.el.to.UserTO", true, new ClassLoader() {
-            @Override
-            public Class<?> loadClass(String name) throws ClassNotFoundException {
-                return super.loadClass(name);
-            }
-        });
-        LOG.info("clazz:{}",clazz);
+        LOG.info("****************************************");
+        Class<?> clazz = Class.forName("com.el.ReflectTest");
+        LOG.info("classLoader:{}",clazz.getClassLoader());
+        LOG.info("clazz:{}",clazz.getName());
 
     }
 
